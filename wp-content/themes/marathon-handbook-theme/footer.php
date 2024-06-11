2e808013771bd62e04bf16667e03059b148f1d63
eternal-hero <nicolasnorth.tech@gmail.com>
2024-06-11 13:09:46 -0500

    'marason'

---
+++ wp-content/themes/marathon-handbook-theme/footer.php
@@ -0,0 +1,7 @@
+    </main>
+    <footer class="mt-10 mx-[15px] sm:mx-[40px]">
+        <p class="text-center">&copy; <?php echo date('Y'); ?> Marathon Handbook. All rights reserved.</p>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
